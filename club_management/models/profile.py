8654f3731c7392e24e136e0d99b3733ca6bdb9fe
Jonathan Sumabat <diaxaxx@gmail.com>
2018-11-11 10:22:26 -0500

    Implement sortedm2m for clubs. User profiles have self-description and the clubs they have joined. Added sortedm2m to settings.py

--- club_management/models/profile.py
+++ club_management/models/profile.py
@@ -5,13 +5,20 @@ from django.utils import timezone
 from django.core.validators import RegexValidator
 from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.models import User
+from sortedm2m.fields import SortedManyToManyField
+
+from club_management.models.clubs import Club
 
 class Profile(models.Model):
     user_name = models.OneToOneField(User, verbose_name=_('Username'), on_delete=models.CASCADE, null=True)
     first_name = models.CharField(max_length=200, blank=True,
-        validators=[RegexValidator('^[a-zA-Z]+$', _('First name must be made of characters only.'))])
+        validators=[RegexValidator('^[a-zA-Z]+$', _('First name must be made of characters only.'))],
+        null=True)
     second_name = models.CharField(max_length=200, blank=True,
-        validators=[RegexValidator('^[a-zA-Z]+$', _('Second name must be made of characters only.'))])
+        validators=[RegexValidator('^[a-zA-Z]+$', _('Second name must be made of characters only.'))],
+        null=True)
+    self_description = models.TextField(verbose_name=_('self description'), null=True, blank=True)
+    clubs = SortedManyToManyField(Club, verbose_name=_('club'), blank=True)
 
     def __str__(self):
         if self.user_name:
