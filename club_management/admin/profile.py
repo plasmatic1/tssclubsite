602cab255c8a9d823e88295257b185c2ff77bdf2
Jonathan Sumabat <diaxaxx@gmail.com>
2018-11-12 10:22:30 -0500

    Clean up some code.

--- club_management/admin/profile.py
+++ club_management/admin/profile.py
@@ -5,8 +5,8 @@ from django.utils.translation import ugettext, ugettext_lazy as _
 from club_management.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    fields = ('user_name', 'first_name', 'second_name', 'self_description', 'clubs')
-    
+    fields = ('user_name', 'first_name', 'last_name', 'self_description', 'clubs')
+
     def get_readonly_fields(self, request, obj=None):
         ret = self.readonly_fields
         if not request.user.has_perm('club_management.edit_joined_clubs'):
