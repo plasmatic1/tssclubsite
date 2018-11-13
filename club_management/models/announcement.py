99dc53b4297c43ab3378d9ad606bc7d79819c5f0
Jonathan Sumabat <diaxaxx@gmail.com>
2018-11-13 10:44:31 -0500

    Start the announcements model.

---
+++ club_management/models/announcement.py
@@ -0,0 +1,27 @@
+import datetime
+
+from django.db import models
+from django.utils import timezone
+from django.core.validators import RegexValidator
+from django.utils.translation import ugettext_lazy as _
+from django.contrib.auth.models import User
+from sortedm2m.fields import SortedManyToManyField
+
+class Announcement(models.Model):
+    code = models.CharField(max_length=20, unique=True, blank=True,
+        validators=[RegexValidator('^[a-z0-9]+$', _('Club code name must be made of characters and/or numbers.'))])
+    title = models.CharField(max_length=100,
+        verbose_name=_('announcement title'))
+    is_public = models.BooleanField(db_index=True, default=False,
+        verbose_name=_('publicly visible'))
+    date_posted = models.DateTimeField(verbose_name=_('published on'))
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        permissions = (
+            ('public_announcement', 'Change is_public field for announcements'),
+        )
+        verbose_name=_('announcement')
+        verbose_name_plural=_('announcements')
